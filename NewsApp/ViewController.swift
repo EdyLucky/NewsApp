6e984f3b24f2a06e5840dc1520c0850c14414e6a
Edy <elshad040@gmail.com>
2019-05-20 22:01:11 +0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Edy on 5/20/19.
+//  Copyright © 2019 Edy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
