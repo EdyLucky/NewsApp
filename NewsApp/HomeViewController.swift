c3224525c7c453f6473504bf952e4e29f1a24c7d
Edy <elshad040@gmail.com>
2019-05-20 23:42:25 +0400

    getNewsData method has been developed for getting JSON data

--- NewsApp/HomeViewController.swift
+++ NewsApp/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  NewsApp
 //
 //  Created by Elshad on 5/20/19.
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class HomeViewController: UIViewController {
+    
+    let API_KEY = "cedf1927835348f7bd03d2f2126cb64c"
+    let URL = "https://newsapi.org/v2/top-headlines"
+    var params : [String:String] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Update URL with api_key
+        params = ["country" : "us", "apiKey" : API_KEY]
+        
+        getNewsData(url: URL, parameters: params)
     }
+    
+    //MARK: - Networking
+    
+    // getNewsData method
+    
+    func getNewsData(url: String, parameters: [String: String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success! Got the news data")
+                let newsJSON : JSON = JSON(response.result.value!)
+                print(newsJSON)
+            } else {
+                print("Error \(String(describing: response.result.error))")
+            }
+        }
+    }
+    
+    
+    //MARK: - JSON Parsing
+    
+    
+    
+    //MARK: - Update UI
+    
+    
+    
 
 
 }
